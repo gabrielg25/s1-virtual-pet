86f6e0ccb656420488de09de45d6222f67baac2a
gabriel guy <gabrielg25@ausdk12.org>
2023-10-11 12:35:36 -0700

    commit

--- cam-v1/Runner.java
+++ cam-v1/Runner.java
@@ -4,19 +4,57 @@ import javax.swing.JOptionPane;
 public class Runner{
     public Runner(){
        VirtualPet v1 = new VirtualPet();
-    v1.exercise();
+    
+    
+       v1.sad();
     takeABeat(1000);
-    v1.feed();
+    String b = getAnswer("I'm sorry");
+    if(b.equals("That was the last straw")){
+v1.shocked();
+     String a = getAnswer("Please don't kill me");
+     if(a.equals("BANG")){
+        v1.die();
+        takeABeat(10000);
+        v1.skeleton();
+         takeABeat(10000);
+         v1.grave();
+          takeABeat(10000);
+        v1.angel();
+     }
+    }else{
+        if(b.equals("I'm in a good mood")){
+            v1.joyful();
+             takeABeat(10000);
+             v1.love();
+              takeABeat(1000);
+              v1.hungry();
+               takeABeat(1000);
+               String c = getAnswer("I'm hungry");
+               if(c.equals("Shut up")){
+                v1.starving();
+                 takeABeat(10000);
+                 String d = getAnswer("Please I'm starving");
+                 if(d.equals("fine eat this"));
+                 v1.feed();
+                  takeABeat(1000);
+                  v1.sick();
+                   takeABeat(1000);
+                   v1.verysick();
+               }
+
+
+        }
+    }
 
     }
     public String getAnswer(String q){
       String s = (String)JOptionPane.showInputDialog(
                     new JFrame(),
-                    "Your Message",
-                    "Your Title",
+                    q,
+                    "Get a Response",
                     JOptionPane.PLAIN_MESSAGE
 );
-if(s.equals(“Yes”) ....
+return s;
 
 
     }
--- cam-v1/VirtualPet.java
+++ cam-v1/VirtualPet.java
@@ -35,5 +35,41 @@ public class VirtualPet {
         hunger = hunger + 1;
         face.setImage("asleep");
     }
+    public void die(){
+        face.setImage("dead");
+    }
+    public void sad(){
+        face.setImage("sad");
+    }
+    public void angel(){
+        face.setImage("angel");
+    }
+    public void shocked(){
+        face.setImage("shocked");
+    }
+    public void skeleton(){
+        face.setImage("skeleton");
+    }
+    public void grave(){
+        face.setImage("pushingdaisies");
+    }
+    public void joyful(){
+        face.setImage("joyful");
+    }
+    public void love(){
+        face.setImage("love");
+    }
+    public void hungry(){
+        face.setImage("hungry");
+    }
+    public void starving(){
+        face.setImage("starving");
+    }
+    public void sick(){
+        face.setImage("sick");
+    }
+    public void verysick(){
+        face.setImage("verysick");
+    }
 
 } // end Virtual Pet
